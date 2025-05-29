7e8d9be5be3d222adc8dffbc4eeb2dd70b50a7a4
itcast <1941125220@qq.com>
2025-05-29 17:03:20 +0800

    xx

--- lib/database.ts
+++ lib/database.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "8888",
+    password: "root",
     database: "user",
     synchronize: true,
     logging: false,
