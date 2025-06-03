b37448ac7ab4fb1e2853b878d3db6425a0811b1a
小小 <12459035+xiaogxyh@user.noreply.gitee.com>
2025-06-03 14:26:08 +0800

    fix(database): 修改数据库密码
    - 将数据库密码从 'root' 修改为 '123456'
    - 此修改提高了数据库的安全性

--- lib/database.ts
+++ lib/database.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "root",
+    password: "123456",
     database: "user",
     synchronize: true,
     logging: false,
