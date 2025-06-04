62f8f8af2fef707898b4a96b640c02adc3a0417c
DSADGF123 <2636530095@qq.com>
2025-06-04 17:08:00 +0800

    完成提交记录按钮

--- lib/database.ts
+++ lib/database.ts
@@ -3,17 +3,19 @@ import { DataSource } from "typeorm";
 import { User } from "./entities/User";
 import { Exam } from "./entities/Exam";
 import { Question } from "./entities/Question";
+import { ExamRecord } from "./entities/ExamRecord";
+
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "8888",
+    password: "000000",
     database: "user",
     synchronize: true,
     logging: false,
-    entities: [User, Exam,Question],
+    entities: [User, Exam,Question,ExamRecord],
     migrations: [],
     subscribers: [],
 });
