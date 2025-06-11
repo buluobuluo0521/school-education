a050b165dc5ce44f4b0e2c864d06bbb81c01a193
itcast <1941125220@qq.com>
2025-06-11 14:05:13 +0800

    完成注册页面

--- lib/database.ts
+++ lib/database.ts
@@ -9,7 +9,7 @@ export const AppDataSource = new DataSource({
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "8888",
+    password: "root",
     database: "user",
     synchronize: true,
     logging: false,
